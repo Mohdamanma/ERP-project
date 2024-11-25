a224c614e6a5282f7c8c1baefaa0f8be87b98f1c
muhammed aman ma <112202255+Mohdamanma@users.noreply.github.com>
2024-11-25 17:57:38 +0530

    versel error solved

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/login',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
